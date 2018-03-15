ec04f46aab59ec39f89884382ed3364d4084fa0d
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-03-15 12:26:13 -0300

    Adicionada função ehPrimo

--- Primo.c
+++ Primo.c
@@ -2,7 +2,22 @@
 
 //Verifica se o número é primo
 
+int ehPrimo(int num);
+
 int main()
 {
 	
+}
+
+int ehPrimo(int num){
+	int i, cont=0;
+	for(i=1; i<=num; i++){
+		if(num % i == 0){
+			cont++;
+		}
+	}
+	if(cont == 2){
+		return 1;
+	}
+	return 0;
 }
